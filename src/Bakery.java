417fecc421bd2967b7aa603a3adeacdacc44a05e
Tracy Zhou <TracyZ14@nycstudents.net>
2024-04-15 14:01:58 -0400

    Deleted the BusinessInfo class and moved the methods to the Business and Bakery class Created the Simulator class to experiment with CardLayout

--- src/Bakery.java
+++ src/Bakery.java
@@ -2,6 +2,21 @@ public class Bakery extends Business
 {
     public Bakery(String name, int employeeWage, boolean ownBuilding, boolean isHiring)
     {
-        super(name, employeeWage, ownBuilding, BusinessInfo.findBakeryRent(), isHiring);
+        super(name, employeeWage, ownBuilding, isHiring);
+    }
+
+    public int startUpCost()
+    {
+        return((int) (51500 + (Math.random() * 90000)));
+    }
+
+    public int buildingCost()
+    {
+        return((int) (267000 + (Math.random() * 483000)));
+    }
+
+    public int buildingRent()
+    {
+        return((int) (30 + (Math.random() * 170)));
     }
 }
--- src/Business.java
+++ src/Business.java
@@ -1,5 +1,4 @@
-public class Business
-{
+public class Business {
     private String name;
     private int employees;
     private int employeeWage;
@@ -7,68 +6,70 @@ public class Business
     private int buildingRent;
     private boolean isHiring;
 
-    public Business(String name, int employeeWage, boolean ownBuilding, int buildingRent, boolean isHiring)
-    {
+    public Business(String name, int employeeWage, boolean ownBuilding, boolean isHiring) {
         this.name = name;
         this.employees = 0;
         this.employeeWage = employeeWage;
         this.ownBuilding = ownBuilding;
-        this.buildingRent = buildingRent;
         this.isHiring = isHiring;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public void setName(String newName)
-    {
+    public void setName(String newName) {
         name = newName;
     }
 
-    public int getEmployees()
-    {
+    public int getEmployees() {
         return employees;
     }
 
-    public void setEmployees(int newEmployees)
-    {
+    public void setEmployees(int newEmployees) {
         employees = newEmployees;
     }
 
-    public int getEmployeeWage()
-    {
+    public int getEmployeeWage() {
         return employeeWage;
     }
 
-    public void setEmployeeWage(int newEmployeeWage)
-    {
+    public void setEmployeeWage(int newEmployeeWage) {
         employeeWage = newEmployeeWage;
     }
 
-    public boolean ownBuilding()
-    {
+    public boolean ownBuilding() {
         return ownBuilding;
     }
 
-    public void setOwnBuilding(boolean newOwnBuilding)
-    {
+    public void setOwnBuilding(boolean newOwnBuilding) {
         ownBuilding = newOwnBuilding;
     }
 
-    public int getBuildingRent()
-    {
+    public int getBuildingRent() {
         return buildingRent;
     }
 
-    public boolean getIsHiring()
-    {
+    public boolean getIsHiring() {
         return isHiring;
     }
 
-    public void setHiring(boolean newIsHiring)
-    {
+    public void setHiring(boolean newIsHiring) {
         isHiring = newIsHiring;
+    }
+
+    public int startUpCost()
+    {
+        return 0;
+    }
+
+    public int buildingCost()
+    {
+        return 0;
+    }
+
+    public int buildingRent()
+    {
+        return 0;
     }
 }
